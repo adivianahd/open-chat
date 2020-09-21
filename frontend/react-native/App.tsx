9b3ded89ac5fb34a73a45096e88dd911e1848ea5
adivianahd <adivianahd07@gmail.com>
2020-09-21 16:04:05 -0300

    create appNavigator, add Screens folder, create Home and Rooms screens

--- frontend/react-native/App.tsx
+++ frontend/react-native/App.tsx
@@ -1,42 +1,14 @@
-import React from 'react';
-import { StyleSheet, View, Text } from 'react-native';
-import Card from './src/components/Card';
+import { createAppContainer } from 'react-navigation';
+import { createStackNavigator } from 'react-navigation-stack';
+import { Home , Rooms } from './src/screens';
 
-const onPress = () => {
-  console.log("este es el onPress");
-}
-
-const App = () => {
-  return (
-    <>
-      <View style={styles.engine}>
-        <Card elevation={1} onPress={onPress}>
-          <Text style={styles.TextStyle}>Soy una Card</Text>
-        </Card>
-      </View>
-    </>
-  );
-};
-
-const styles = StyleSheet.create({
-  engine: {
-    flex: 1,
-    alignItems: 'stretch',
-    justifyContent: 'center',
+const AppNavigator = createStackNavigator({
+  Home: {
+    screen: Home
   },
-
-  TextStyle: {
-    fontSize: 18,
+  Rooms: {
+    screen: Rooms
   },
+},{ initialRouteName: 'Home'})
 
-  footer: {
-    color: '#222',
-    fontSize: 12,
-    fontWeight: '600',
-    padding: 4,
-    paddingRight: 12,
-    textAlign: 'right',
-  },
-});
-
-export default App;
+export default createAppContainer(AppNavigator);
---
+++ frontend/react-native/src/screens/Rooms.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+const Rooms = () => {
+  return (
+    <View>
+      <Text>Salas de chat</Text>
+    </View>
+  )
+};
+
+Rooms.navigationOptions = {
+  title: 'Salas',
+};
+
+export default Rooms;
