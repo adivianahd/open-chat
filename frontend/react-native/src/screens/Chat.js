3b302eddf5ae5c9c7b2f4020a984b9d88fe9055e
adivianahd <adivianahd07@gmail.com>
2020-09-23 10:51:03 -0300

    add screen chat

---
+++ frontend/react-native/src/screens/Chat.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+const Chat = () => {
+  return (
+    <View>
+      <Text>chat seleccionado</Text>
+    </View>
+  )
+};
+
+Chat.navigationOptions = {
+  title: 'Chat',
+};
+
+export default Chat;
